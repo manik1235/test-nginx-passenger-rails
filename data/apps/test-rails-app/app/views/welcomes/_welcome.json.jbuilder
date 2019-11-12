3090fb322e9db1524acccd8bc5a4df74d7567d78
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2019-11-11 19:56:15 -0500

    Add `welcomes` scaffold to rails app
    This commit adds a scaffold called `welcomes` to the rails app, and sets
    the root path to go to it's index action.

---
+++ data/apps/test-rails-app/app/views/welcomes/_welcome.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! welcome, :id, :name, :words, :created_at, :updated_at
+json.url welcome_url(welcome, format: :json)
---
+++ data/apps/test-rails-app/db/migrate/20191112005421_create_welcomes.rb
@@ -0,0 +1,10 @@
+class CreateWelcomes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :welcomes do |t|
+      t.string :name
+      t.string :words
+
+      t.timestamps
+    end
+  end
+end
